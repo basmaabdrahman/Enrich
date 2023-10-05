c7fd4694b1e2d4c7a05e87dee3a5e5105dc2b1a9
basmaabdrahman <90579855+basmaabdrahman@users.noreply.github.com>
2023-10-05 19:34:54 +0300

    Update StoreCourseRequest.php

--- app/Http/Requests/StoreCourseRequest.php
+++ app/Http/Requests/StoreCourseRequest.php
@@ -11,10 +11,7 @@ class StoreCourseRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
-    {
-        return false;
-    }
+   
 
     /**
      * Get the validation rules that apply to the request.
